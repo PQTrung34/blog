51a33b4ddab61b6b0a6a3e343c9f10426ff95a8c
pqt <pqtrung34@gmail.com>
2024-11-06 21:06:00 +0700

    create post model

--- app/views.py
+++ app/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello world</h1>')
+
+def home(request):
+    context = {}
+    return render(request, 'home.html', context)
