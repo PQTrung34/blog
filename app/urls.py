ce9eb02978ac1cd08f625aa65933e7dce910ee8d
pqt <pqtrung34@gmail.com>
2024-11-06 23:19:50 +0700

    get post

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.HomeView.as_view(), name='home'),
+    path('article/<int:pk>', views.ArticleDetail.as_view(), name='article-detail')
 ]
--- app/views.py
+++ app/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.views.generic import ListView, DetailView
+from .models import Post
 # Create your views here.
 def index(request):
     return HttpResponse('<h1>Hello world</h1>')
 
-def home(request):
-    context = {}
-    return render(request, 'home.html', context)
+class HomeView(ListView):
+    model = Post
+    template_name = 'home.html'
+
+class ArticleDetail(DetailView):
+    model = Post
+    template_name = 'article_detail.html'
